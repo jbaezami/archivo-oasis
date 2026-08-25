5f4b66b9a29b711e900236ec9fac4b52acb2092d
juankllr <jbaezam@unex.es>
2026-08-25 09:46:02 +0200

    Add App shell, routing and Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+
+function Home() {
+  return (
+    <main className={styles.container}>
+      <h1>Bienvenido al almanaque</h1>
+    </main>
+  )
+}
+
+export default Home
